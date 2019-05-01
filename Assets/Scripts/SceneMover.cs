d082c04b11b451706fc289f0d2f0775ac2408be8
parkerhams <parker.hamilton@loop.colum.edu>
2019-05-01 12:56:08 -0500

    FINSIHED PROJECTS

---
+++ Assets/Scripts/SceneMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMover : MonoBehaviour {
+    ScreenFader fadeScr;
+    //public int SceneNumb;
+
+    void Awake()
+    {
+        fadeScr = GameObject.FindObjectOfType<ScreenFader>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //fadeScr.EndScene(SceneNumb);
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
---
+++ Assets/Scripts/TerrainRespawnPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TerrainRespawnPlayer : MonoBehaviour {
+    float playerHeight;
+    [SerializeField] float deathHeight;
+    GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        playerHeight = player.transform.position.y;
+	}
+
+    //public void OnCollisionEnter(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //    }
+    //}
+
+    public void RespawnTheThing()
+    {
+        if (playerHeight <= deathHeight)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
